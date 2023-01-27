0c04bb5c3b77c14886014b408c5f2215b0516202
Wouter Scherphof <wouter.scherphof@gmail.com>
2023-01-27 12:10:14 +0100

    wip pipeline

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-IMAGE=${IMAGE:-docker4gis/proxy}
-DOCKER_BASE=$(npx --yes docker4gis@"${DOCKER4GIS_VERSION:-latest}" base)
-DOCKER_USER=$DOCKER_USER
-
-mkdir -p conf
-cp -r "$DOCKER_BASE"/.plugins "$DOCKER_BASE"/.docker4gis conf
 docker image build \
 	--build-arg DOCKER_USER="$DOCKER_USER" \
 	-t "$IMAGE" .
-rm -rf conf/.plugins conf/.docker4gis
