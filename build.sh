378009af48b54c126eea92ad03dc1f4d1d63e207
Azure Pipeline <pipeline@azure.com>
2025-02-14 23:03:49 +0100

    dg bump, fix authorise StatusCode

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker image build \
+	--build-arg DOCKER_REGISTRY="$DOCKER_REGISTRY" \
 	--build-arg DOCKER_USER="$DOCKER_USER" \
-	-t "$IMAGE" .
+	--build-arg DOCKER_REPO="$DOCKER_REPO" \
+	-t "$DOCKER_IMAGE" .
