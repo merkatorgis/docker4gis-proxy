0c0d0b343e9816f55c89d6c0137c25e62d382ed3
Wouter <wouter.scherphof@gmail.com>
2026-03-02 14:51:37 +0100

    fix: undo SSL constraints changes
    Since score at ssllabs went from A+ to A.
    And the issue was resolved apart form these changes.

--- goproxy/main.go
+++ goproxy/main.go
@@ -131,7 +131,15 @@ func main() {
 		// Configure TLSConfig following recommendations from
 		// https://www.ssllabs.com/ssltest.
 		tlsConfig := manager.TLSConfig()
-		tlsConfig.MinVersion = tls.VersionTLS13
+		tlsConfig.MinVersion = tls.VersionTLS12
+		tlsConfig.CipherSuites = []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		}
 
 		server := &http.Server{
 			Addr:      ":https",
